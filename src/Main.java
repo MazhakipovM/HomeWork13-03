bffde8d07414656e136fe4c3df26df85f02adde7
marat <marat.forsky@gmail.com>
2023-03-19 20:13:48 +0300

    hw done

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,45 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+
+
+        task1();
+    }
+
+    private static void task1() {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("What is your first name ?");
+        String firstName = scan.next();
+        System.out.println("What is your middle name ?");
+        String middleName = scan.next();
+        System.out.println("What is your last name ?");
+        String lastName = scan.next();
+        String fullname = lastName +" "+ firstName+" "+middleName;
+
+        System.out.println("ФИО сотрудника — "+ fullname);
+
+
+        task2(fullname);
+
+
+    }
+
+    private static void task2(String fullname) {
+        System.out.println("ФИО сотрудника — "+ fullname.toUpperCase());
+
+
+        task3(fullname);
+
+
+    }
+
+    private static void task3(String fullname) {
+        fullname = fullname.replace("ё","е");
+        System.out.println("Данные ФИО сотрудника — " + fullname);
     }
 }
+
+
